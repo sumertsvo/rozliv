224891b2cac92d5ecf660ba4dcffabeea3a42d23
vlad <vlad.s.o@yandex.ru>
2020-02-05 07:08:19 +0300

    4 ver

--- prog.ino
+++ prog.ino
@@ -8,55 +8,60 @@
 //-------------------------------—
 
 //разница массы насоса
-const int DM_NASOS = 100;
-//разница массы насоса
-const int M_CALIBR = 1000;
+const int DM_NASOS = 1;
 //количество
 const byte KOLVO = 4;
+
 //включение отладки
-boolean TEST = true;
+boolean TEST = false;//общий тест
+boolean KTEST = false;//тест кнопок
+boolean VTEST = false;//тест процедуры ves
+boolean STEST = false;//тест самих сенсоров
+boolean CTEST = false;//тест калибровки
+boolean NTEST = false;//тест налива
+boolean HTEST = false;//тест прогрева
 
 //время вертикалки
 const word T_VERT = 3000;
 //время горизонталки
-const word T_GORIZ = 3000;
+const word T_GORIZ = 1;
 //---------------------------------
 
 //пины датчиков
-Q2HX711 datch0(A7, A6);
-Q2HX711 datch1(A5, A4);
-Q2HX711 datch2(A3, A2);
-Q2HX711 datch3(A1, A0);
+Q2HX711 datch0(A9, A8);
+Q2HX711 datch1(A4, A5);
+Q2HX711 datch2(A2, A3);
+Q2HX711 datch3(A0, A1);
 //пины дисплеев (SDA,SCL)
 OLED scrn(20, 21);
 //пины реле
-const byte rele[KOLVO] = {41, 43, 45, 47};
+const byte rele[KOLVO] = {7, 6, 5, 4};
 //кнопка
-const byte KNOP = 52;
-const byte GORIZ = 49;
-const byte VERT = 51;
-const byte NASOS = 53;
-const unsigned long T_HEAT = 60000;
+const byte GORIZ = 39;
+const byte VERT = 41;
+const byte NASOS = 43;
+const unsigned long T_HEAT = 3000;
 //пины клавиатуры
-const byte rowPins[4] = {7 , 6, 5, 4};
-const byte colPins[4] = {11, 10, 9, 8} ;
+const byte rowPins[4] = {26 , 24, 22, 31};
+const byte colPins[4] = {34, 32, 30, 28} ;
 //-------------------------------—
 
 //обьявления переменных
 byte rez = 0;//0-ожидание,1-налив,2-калибр,3-прогрев
-byte stg = 0;//0-нет,1-пусто,2-контрольное
+byte stg = 0;//
 unsigned long starttime;
 
 
 String str, msg;
 int num;
+int znach[KOLVO] = {0, 0, 0, 0};
 int mas[KOLVO] = {0, 0, 0, 0};
 int k[KOLVO] = {0, 0, 0, 0};
 boolean on[KOLVO] = {0, 0, 0, 0};
 //коэф расчета
 float koef[KOLVO] = {0.8138379, 0.783257, 0.818425, 0.784786};
 //коэффициенты 0
-int nol[KOLVO] = {8930, 9158, 8780, 8950};
+int nol[KOLVO] = {0, 0, 0, 0};
 
 //-------------------------------—
 
@@ -79,183 +84,244 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, 4, 4 );
 
 
 void setup() {
-  //кнопка вкл
-  pinMode(KNOP, INPUT);
+  //отладочное
+  Serial.begin(9600);
+  //-------------------------------—
+  if (TEST) Serial.print("start");
   //активация дисплеев
   initScreen(scrn);
   //активация реле
   initRele();
   //начальные параметры
   rez = 3;
-  starttime = 0;
+  starttime = 1;
   //-------------------------------—
-  //отладочное
-  if (TEST) {
-    Serial.begin(9600);
-  }
-  //-------------------------------—
+  readZnach(datch0, 0);
+  readZnach(datch1, 1);
+  readZnach(datch2, 2);
+  readZnach(datch3, 3);
+  nol[0] = znach[0];
+  nol[1] = znach[1];
+  nol[2] = znach[2];
+  nol[3] = znach[3];
 }
 
 
 //======================================================================================
 
 void loop() {
-  static boolean flag;
+  //переменые
   char key;
-  static int mc1, mc2;
+  byte q;
+  static int m_calibr;
+  //считываем значения
+  readZnach(datch0, 0);
+  readZnach(datch1, 1);
+  readZnach(datch2, 2);
+  readZnach(datch3, 3);
+  if (TEST) Serial.print("rez=");
+  if (TEST) Serial.println(rez);
   switch (rez) {
-
     //налив
+    //==================================================================================
     case 1:
-      ves(datch0, 0);
-      ves(datch1, 1);
-      ves(datch2, 2);
-      ves(datch3, 3);
-      if (dm >= DM_NASOS) {
-        if (updateRele) {
-          if (TEST) Serial.println("работает");
+      for (byte i = 0; i < KOLVO; i++) {
+        ves(i);
+      }
+      if (dm() >= DM_NASOS) {
+        if (updateRele()) {
+          if (NTEST) Serial.println("flow");
         }
         else {
-          stopFlow;
+          if (NTEST) Serial.println("stop flow");
+          stopFlow();
           rez = 0;
         }
-      } else {
+      }
+      else {
         digitalWrite(NASOS, 0);
-        if (TEST) Serial.println("насос вкл");
+        if (NTEST) Serial.println("nasos vkl");
+      }
+      if (NTEST) {
+        for (byte i = 0; i < KOLVO; i++) {
+          Serial.print("rel_");
+          Serial.println(i + 1);
+          if (on[i])
+            Serial.println("on");
+          else Serial.println("off");
+        }
+        Serial.print("dm=");
+        Serial.println(dm());
+        Serial.print("DM_NASOS=");
+        Serial.println(DM_NASOS);
       }
       refreshScreen(scrn, num, "Flow");
       break;
-
-
-
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     //калибровка
+    //==================================================================================
     case 2:
-      //кнопка
-      if (digitalRead(KNOP) == HIGH && flag == 0) //если кнопка нажата
-      {
-        flag = 1;
-
-        switch (stg) {
-
-          case 1:
-            ves(datch0, 0);
-            mc1 = mas[0];
-            break;
-
-          case 2:
-            ves(datch0, 0);
-            mc2 = mas[0];
-            koef[0] = mc2 - mc1 / M_CALIBR;
-            break;
-
-          case 3:
-            ves(datch1, 1);
-            mc1 = mas[1];
-            break;
-
-          case 4:
-            ves(datch1, 1);
-            mc2 = mas[1];
-            koef[1] = mc2 - mc1 / M_CALIBR;
-            break;
-
-          case 5:
-            ves(datch2, 2);
-            mc1 = mas[2];
-            break;
-
-          case 6:
-            ves(datch2, 2);
-            mc2 = mas[2];
-            koef[2] = mc2 - mc1 / M_CALIBR;
-            break;
-
-          case 7:
-            ves(datch3, 3);
-            mc1 = mas[3];
-            break;
-
-          case 8:
-            ves(datch3, 3);
-            mc2 = mas[3];
-            koef[3] = mc2 - mc1 / M_CALIBR;
-            break;
-
-          default:
-            rez = 0;
-            break;
-        }
-
-      }
-      if (digitalRead(KNOP) == LOW && flag == 1) //если кнопка отжата
-      {
-        flag = 0; //обнуляем переменную flag
-      }
-
-      refreshScreen(scrn, stg, "Calibr. stage:");
-      break;
-
-
-    //прогрев
-    case 3:
-      if (millis < starttime + T_HEAT) {
-        ves(datch0, 0);
-        ves(datch1, 1);
-        ves(datch2, 2);
-        ves(datch3, 3);
-        refreshScreen(scrn, 0, "Heat");
-      }
-      break;
-
-    //ожидание
-    default:
-      //кнопка
-      if (digitalRead(KNOP) == HIGH && flag == 0) //если кнопка нажата
-      {
-        flag = 1;
-        // защита от "дребезга" 100%
-        startFlow;
-        rez = 1;
-      }
-      if (digitalRead(KNOP) == LOW && flag == 1) //если кнопка отжата
-      {
-        flag = 0; //обнуляем переменную flag
-      }
-      //==========================================================================
-
       //считывание клавиатуры
       key = keypad.getKey();
       if (key) {
-        if (TEST) Serial.println(key); // Передаем нажатую клавишу в сериал порт (отладочное)
+        if (KTEST) Serial.println(key); // Передаем нажатую клавишу в сериал порт (отладочное)
         switch (key) {
           case 'A':
-            for (byte i = 0; i < 4; i++) {
-              on[i] = !on[i];
-            }
-            delay(100);
+          stg = 5;
             break;
           case 'B':
+            stg++;
+            switch (stg) {
+              case 0:
+                break;
+              case 1:
+                q = stg - 1;
+                mas[q] = znach[q];
+                koef[q] = (float(mas[q] - nol[q]) / m_calibr);
+                break;
+
+              case 2:
+                q = stg - 1;
+                mas[q] = znach[q];
+                koef[q] = (float(mas[q] - nol[q]) / m_calibr);
+                break;
+              case 3:
+                q = stg - 1;
+                mas[q] = znach[q];
+                koef[q] = (float(mas[q] - nol[q]) / m_calibr);
+                break;
+              case 4:
+                q = stg - 1;
+                mas[q] = znach[q];
+                koef[q] = (float(mas[q] - nol[q]) / m_calibr);
+                break;
+              default:
+                rez = 0;
+                stg = 0;
+                str = '0';
+                break;
+            }
 
             break;
           case 'C':
+            stg = 0;
+                for (byte i = 0; i < KOLVO; i++) {
+              mas[i] = 0;
+            }
+            break;
+          case 'D':
+            nol[0] = znach[0];
+            nol[1] = znach[1];
+            nol[2] = znach[2];
+            nol[3] = znach[3];
+            break;
+          case '*':
+            stg++;
+            break;
+          case '#':
+            stg--;
+            break;
+          default:
+            str = str + key;
+            if (stg == 0) m_calibr = str.toInt();
+            break;
+        }
+      }
+      if (CTEST) {
+        Serial.print("m_calibr=");
+        Serial.println(m_calibr);
+        Serial.print("mas1=");
+        Serial.println(mas[0]);
+        Serial.print("nol1=");
+        Serial.println(nol[0]);
+        Serial.print("koef1=");
+        Serial.println(koef[0]);
+        Serial.print("mas2=");
+        Serial.println(mas[1]);
+        Serial.print("nol2=");
+        Serial.println(nol[1]);
+        Serial.print("koef2=");
+        Serial.println(koef[1]);
+        Serial.print("mas3=");
+        Serial.println(mas[2]);
+        Serial.print("nol3=");
+        Serial.println(nol[2]);
+        Serial.print("koef3=");
+        Serial.println(koef[2]);
+        Serial.print("mas4=");
+        Serial.println(mas[3]);
+        Serial.print("nol4=");
+        Serial.println(nol[3]);
+        Serial.print("koef4=");
+        Serial.println(koef[3]);
+      }
+      if (stg == 0) {
+        refreshScreen(scrn, m_calibr, "Calibr. mas:");
+      }
+      else {
+        refreshScreen(scrn, stg, "Calibr. stage:");
+      };
+      break;
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //прогрев
+    //==================================================================================
+    case 3:
+      if (HTEST) Serial.println(millis());
+      if (millis() < T_HEAT) {
+        refreshScreen(scrn, 0, "Heat");
+      } else {
+        rez = 0;
+      }
+      break;
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //ожидание
+    //==================================================================================
+    default:
+      //считывание клавиатуры
+      key = keypad.getKey();
+      if (key) {
+        if (KTEST) Serial.println(key); // Передаем нажатую клавишу в сериал порт (отладочное)
+        switch (key) {
+          case 'A':
+            rez = 2;
+            stg = 0;
+            str = '0';
+            for (byte i = 0; i < KOLVO; i++) {
+              mas[i] = 0;
+            }
+            for (byte i = 0; i < KOLVO; i++) {
+              nol[i] = znach[i];
+            }
+            break;
+
+          case 'B':
+            startFlow();
+            rez = 1;
+            break;
+
+          case 'C':
             str = "";
             num = 0;
-            for (byte i = 0; i < 4; i++) {
+            for (byte i = 0; i < KOLVO; i++) {
               mas[i] = 0;
               k[i] =  0;
               on[i] = 0;
             }
             break;
+
           case 'D':
-            k[0] = int(datch0.read() / 1000) - nol[0];
-            k[1] = int(datch1.read() / 1000) - nol[1];
-            k[2] = int(datch2.read() / 1000) - nol[2];
-            k[3] = int(datch3.read() / 1000) - nol[3];
+            nol[0] = znach[0];
+            nol[1] = znach[1];
+            nol[2] = znach[2];
+            nol[3] = znach[3];
             break;
+
           case '*':
             break;
+
           case '#':
             break;
+
           default:
             str = str + key;
             num = str.toInt();
@@ -281,6 +347,7 @@ void loop() {
 
 //процедура обновления экранов
 void refreshScreen(OLED & myOLED, int i, String s) {
+  if (TEST) Serial.print("refreshScreen");
   myOLED.clrScr();
   myOLED.setFont (BigNumbers);
   myOLED.printNumI(i, CENTER, 30);
@@ -293,6 +360,7 @@ void refreshScreen(OLED & myOLED, int i, String s) {
 
 //процедура активации экранов
 void initScreen(OLED & myOLED) {
+  if (TEST) Serial.print("initScreen");
   myOLED.begin();
   myOLED.clrScr();
   myOLED.setFont(BigNumbers);
@@ -302,6 +370,13 @@ void initScreen(OLED & myOLED) {
 
 //процедура активации реле
 void initRele() {
+  if (TEST) Serial.print("initRele");
+  pinMode(GORIZ, OUTPUT);
+  digitalWrite(GORIZ, 1);
+  pinMode(VERT, OUTPUT);
+  digitalWrite(VERT, 1);
+  pinMode(NASOS, OUTPUT);
+  digitalWrite(NASOS, 1);
   for (byte i = 0; i < KOLVO; i++) {
     pinMode(rele[i], OUTPUT);
     digitalWrite(rele[i], 1);
@@ -312,6 +387,7 @@ void initRele() {
 
 //процедура переключения реле
 boolean updateRele() {
+  if (TEST) Serial.print("updateRele");
   boolean buf = false;
   for (byte i = 0; i < KOLVO; i++) {
     digitalWrite(rele[i], !on[i]);
@@ -324,6 +400,7 @@ boolean updateRele() {
 
 //процедура для насоса
 int dm() {
+  if (TEST) Serial.print("dm");
   static int delta[KOLVO] = {0, 0, 0, 0};
   int dx = 0;
   for (byte i = 0; i < KOLVO; i++) {
@@ -340,56 +417,75 @@ int dm() {
 
 //процедура пуска
 void startFlow() {
-  for (byte i = 0; i < 4; i++) {
+  if (TEST) Serial.print("startFlow");
+  for (byte i = 0; i < KOLVO; i++) {
     mas[i] = 0;
     k[i] =  0;
-    on[i] = 0;
+    on[i] = 1;
   }
   digitalWrite(GORIZ, 0);
   delay(T_GORIZ);//горизонтальная задержка
-  if (TEST) Serial.println("горизонт на месте");
+  if (NTEST) Serial.println("gorizont na meste");
   digitalWrite(VERT, 0);
   delay(T_VERT);//вертикальная задержка
-  if (TEST) Serial.println("вертикаль на месте");
-  //сброс веса
-  k[0] = int(datch0.read() / 1000) - nol[0];
-  k[1] = int(datch1.read() / 1000) - nol[1];
-  k[2] = int(datch2.read() / 1000) - nol[2];
-  k[3] = int(datch3.read() / 1000) - nol[3];
-  if (TEST) Serial.println("тара сброшена");
+  if (NTEST) Serial.println("vertical na meste");
+  //сброс тары
+  k[0] = znach[0] - nol[0];
+  k[1] = znach[1] - nol[1];
+  k[2] = znach[2] - nol[2];
+  k[3] = znach[3] - nol[3];
+  if (NTEST) Serial.println("tara sbroshena");
+  for (byte i = 0; i < KOLVO; i++) {
+    digitalWrite(rele[i], 0);
+  }
+  if (NTEST) Serial.println("rele vkluch");
 }
 
 //======================================================================================
 
 //процедура завершения
 void stopFlow() {
+  if (TEST) Serial.print("stopFlow");
   digitalWrite(NASOS, 1);
-  if (TEST)   Serial.println("насос выкл");
+  if (NTEST)   Serial.println("nasos vikl");
   digitalWrite(VERT, 1);
   delay(T_VERT);
-  if (TEST)   Serial.println("вертикаль убрана");
+  if (NTEST)   Serial.println("vertical ubrana");
   digitalWrite(GORIZ, 1);
   delay(T_GORIZ);
-  if (TEST)   Serial.println("горизонт убран");
+  if (NTEST)   Serial.println("gorizont ubran");
 }
 
 //======================================================================================
 //процедура просчёта релешек
-void ves(Q2HX711 & datch, int i) {
-  mas[i] = (int(datch.read() / 1000) - (nol[i] + k[i])) / koef[i]; //в конце нужный коэф
-  if (TEST) {
-    Serial.print("показания датчика=");
-    Serial.println(int(datch.read() / 1000));
-    Serial.print("масса=");
-    Serial.println( mas[i]);//масса с учетом тары и т.п.
-    Serial.print("заданный вес=");
-    Serial.println( num);
-    Serial.print("нулевая отметка=");
+void ves(int i) {
+  mas[i] = (znach[i] - (nol[i] + k[i])) / koef[i]; //в конце нужный коэф
+  if (VTEST) {
+    Serial.print("pokazaniya=");
+    Serial.println(znach[i]);
+    Serial.print("mass=");
+    Serial.println(mas[i]);//масса с учетом тары и т.п.
+    Serial.print("zadanniy ves=");
+    Serial.println(num);
+    Serial.print("nulevaya otmetka=");
     Serial.println(nol[i]);
-    Serial.print("добавка тары=");
+    Serial.print("tara=");
     Serial.println(k[i]);
-    Serial.print("коэфф.=");
+    Serial.print("koeff.=");
     Serial.println(koef[i]);
+    if (on[i]) Serial.print("=on");
+    else Serial.print("=off");
     Serial.println( );
+  }
+}
+
+//======================================================================================
+//процедура просчёта релешек
+void readZnach(Q2HX711 & datch, int i) {
+  znach[i] = (int(datch.read() / 1000));
+  if (STEST) {
+    Serial.print("pokazanie_");
+    Serial.println(i + 1);
+    Serial.println(znach[i]);
   }
 }
